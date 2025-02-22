bda6f9ec272965160498f3509c56a218eff72908
Lanrico1 <lanecobb@hotmail.com>
2025-02-22 15:03:42 -0600

    D: Finish asteroid game

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init  # Call init method
@@ -10,6 +13,20 @@ def main():
     # Create Clock object
     clock = pygame.time.Clock()  # Create Clock
     dt = 0  # initialize Delta time variable
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
 
     # Create Player object
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -22,8 +39,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         pygame.Surface.fill(screen, (0, 0, 0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         player.draw(screen)
         pygame.display.flip()
         
